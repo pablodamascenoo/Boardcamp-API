c1662a0871b3166eba7b1057da0ee51c18ae1760
pablodamascenoo <pablofacex@gmail.com>
2022-05-25 17:55:12 -0300

    feat: added router to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { json } from "express";
 import dotenv from "dotenv";
 import chalk from "chalk";
+import router from "./routes/index.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(router);
 
 app.listen(port, () => {
     console.log(chalk.bold.cyan("\n Server running...\n"));
