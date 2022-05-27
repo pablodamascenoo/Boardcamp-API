4e4477d80a037d2796189a4d563741fb6a7d6479
pablodamascenoo <pablofacex@gmail.com>
2022-05-27 20:58:49 -0300

    feat: finish categories routes

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -14,3 +14,13 @@ export async function postCategorie(req, res) {
         return res.sendStatus(500);
     }
 }
+
+export async function getCategories(req, res) {
+    try {
+        const categories = await connection.query("SELECT * FROM categories");
+        return res.status(200).send(categories.rows);
+    } catch (error) {
+        failure(error);
+        return res.sendStatus(500);
+    }
+}
--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,9 +1,13 @@
 import Router from "express";
-import { postCategorie } from "../controllers/categoriesController.js";
+import {
+    postCategorie,
+    getCategories,
+} from "../controllers/categoriesController.js";
 import { validateCategory } from "../middlewares/categoriesMiddleware.js";
 
 const categoriesRouter = Router();
 
 categoriesRouter.post("/categories", validateCategory, postCategorie);
+categoriesRouter.get("/categories", getCategories);
 
 export default categoriesRouter;
