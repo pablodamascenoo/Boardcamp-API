6727fde386d076d53ae015c7e5d334d0f0793aea
pablodamascenoo <pablofacex@gmail.com>
2022-05-27 19:53:08 -0300

    feat: created postCategorie function

---
+++ src/controllers/categoriesController.js
@@ -0,0 +1,16 @@
+import connection from "../db.js";
+import { failure } from "../misc/chalkAlerts";
+
+export async function postCategorie(req, res) {
+    const { name } = res.locals;
+
+    try {
+        await connection.query(`INSERT INTO categories (name) VALUES($1)`, [
+            name,
+        ]);
+        return res.sendStatus(201);
+    } catch (error) {
+        failure(error);
+        return res.sendStatus(500);
+    }
+}
