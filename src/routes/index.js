91fc3d37786ee9fac32dfe3e70a08cec3474b656
pablodamascenoo <pablofacex@gmail.com>
2022-05-27 21:21:47 -0300

    feat: created gamesRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import customersRouter from "./customersRouter.js";
 import categoriesRouter from "./categoriesRouter.js";
+import gamesRouter from "./gamesRouter.js";
 
 const router = Router();
 
 router.use(customersRouter);
 router.use(categoriesRouter);
+router.use(gamesRouter);
 
 export default router;
