e8c22a94eb790ec9cf921c5443eb13e77e2b9f72
pablodamascenoo <pablofacex@gmail.com>
2022-05-29 19:54:41 -0300

    feat: finish /rentals GET route

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -19,3 +19,53 @@ export async function postRental(req, res) {
         return res.sendStatus(500);
     }
 }
+
+export async function getRentals(req, res) {
+    const customerId = req.query.customerId
+        ? `"customerId"=${req.query.customerId}`
+        : "1=1";
+
+    const gameId = req.query.gameId ? `"gameId"=${req.query.gameId}` : "1=1";
+
+    try {
+        const rentals = await connection.query(
+            `
+        SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name as "categoryName"
+        FROM rentals
+        JOIN customers ON customers.id = rentals."customerId"
+        JOIN games ON games.id = rentals."gameId"
+        JOIN categories ON categories.id = games."categoryId" WHERE ${customerId} AND ${gameId}
+        `
+        );
+
+        let formatedRentals = [];
+
+        for (let rental of rentals.rows) {
+            rental = {
+                ...rental,
+                customer: {
+                    id: rental.customerId,
+                    name: rental.customerName,
+                },
+                game: {
+                    id: rental.gameId,
+                    name: rental.gameName,
+                    categoryId: rental.categoryId,
+                    categoryName: rental.categoryName,
+                },
+            };
+
+            delete rental.categoryId;
+            delete rental.categoryName;
+            delete rental.gameName;
+            delete rental.customerName;
+
+            formatedRentals.push(rental);
+        }
+
+        return res.status(200).send(formatedRentals);
+    } catch (error) {
+        failure(error);
+        return res.sendStatus(500);
+    }
+}
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,10 +1,10 @@
 import Router from "express";
-import { postRental } from "../controllers/rentalsController.js";
+import { getRentals, postRental } from "../controllers/rentalsController.js";
 import { verifyRental } from "../middlewares/rentalsMiddleware.js";
 
 const rentalsRouter = Router();
 
-rentalsRouter.get("/rentals");
+rentalsRouter.get("/rentals", getRentals);
 rentalsRouter.post("/rentals", verifyRental, postRental);
 rentalsRouter.post("/rentals/:id/return");
 rentalsRouter.delete("/rentals/:id");
